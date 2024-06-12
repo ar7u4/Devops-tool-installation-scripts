16e2993aaec9af4c8f23c98e464556e07dee3e34
ar7u4 <109585047+ar7u4@users.noreply.github.com>
2024-06-12 12:06:34 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-sudo apt update -y
-
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" -y
-
-sudo apt update -y
-
-apt-cache policy docker-ce -y
-
-sudo apt install docker-ce -y
+$ sudo apt-get update
+ 
+$ sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+$ sudo mkdir -p /etc/apt/keyrings
+$ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+$ echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  
+$ sudo apt-get update
+$ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+$ sudo groupadd docker
+$ sudo usermod -aG docker $USER
 
 #sudo systemctl status docker
 
